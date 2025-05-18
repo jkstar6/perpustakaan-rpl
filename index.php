612341295e0368342a88e2727f2c4f154ca86caa
Bintang Jati <bintangid69@gmail.com>
2025-05-18 13:27:51 +0700

    main page done

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <title>Perpustakaan</title>
+</head>
+
+<body>
+    <nav>
+        <img src="img/logo-nav.png" alt="">
+        <ul>
+            <li><a class="home" href="#">Home</a></li>
+            <li><a href="#">Buku</a></li>
+            <li><a href="#">Petugas</a></li>
+        </ul>
+    </nav>
+
+    <header>
+        <div class="header-left">
+            <h1>Perpustakaan<br>Kampoeng Galia</h1>
+            <p>your text here</p>
+            <button><a class="a-header" href="">jelajahi</a></button>
+        </div>
+        <div class="header-right">
+            <img src="img/header-image.png" alt="">
+        </div>
+
+    </header>
+
+    <main>
+        <h2>Pencarian Buku</h2>
+        <div class="search-form">
+            <img src="img/search.png" alt="">
+            <input type="text">
+            <button>SEARCH</button>
+        </div>
+
+        <div class="container">
+            <div class="box">
+                <div class="frame">
+                    <img src="img/buku.jpg" alt="">
+                </div>
+                <p class="title">Book Title</p>
+                <p class="status">STATUS</p>
+            </div>
+            <div class="box">
+                <div class="frame">
+                    <img src="img/buku.jpg" alt="">
+                </div>
+                <p class="title">Book Title</p>
+                <p class="status">STATUS</p>
+            </div>
+            <div class="box">
+                <div class="frame">
+                    <img src="img/buku.jpg" alt="">
+                </div>
+                <p class="title">Book Title</p>
+                <p class="status">STATUS</p>
+            </div>
+            <div class="box">
+                <div class="frame">
+                    <img src="img/buku.jpg" alt="">
+                </div>
+                <p class="title">Book Title</p>
+                <p class="status">STATUS</p>
+            </div>
+            <div class="box">
+                <div class="frame">
+                    <img src="img/buku.jpg" alt="">
+                </div>
+                <p class="title">Book Title</p>
+                <p class="status">STATUS</p>
+            </div>
+        </div>
+        
+        <button class="selengkapnya"><a href="">Selengkapnya</a></button>
+    </main>
+
+</body>
+
+</html>
